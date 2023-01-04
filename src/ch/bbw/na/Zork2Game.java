d008ec251b88514ab6b24562e42a74f20d153e71
Jorin Heer <jorin.heer@gmail.com>
2023-01-04 16:04:54 +0100

    Initial commit

---
+++ src/ch/bbw/na/Zork2Game.java
@@ -0,0 +1,10 @@
+package ch.bbw.na;
+
+public class Zork2Game {
+
+    public static void main(String[] args) {
+	// write your code here
+        Game Henlo = new Game();
+        Henlo.play();
+    }
+}
